2ac69724f9d7663370cb96e99acf4f53e3b112ea
David Chuvik <dchuvik@gmail.com>
2022-06-13 15:26:13 -0400

    Bank application basic

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount userBankAccount = new BankAccount();
+            Console.WriteLine($"Please input how much you would like to deposit: ");
+            userBankAccount.Deposit(Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine($"Your bank balance is {userBankAccount.GetBalance()}");
+        }
+    }
+}
